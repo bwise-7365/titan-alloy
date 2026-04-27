ea4e30817ee96b431abd0937c34739b0d87c8cd6
Ben Wise <bwise.6bx1@gmail.com>
2026-04-27 15:35:16 -0400

    Got the stones to draw
    Added a TGZ file of Qt dlls to unzip in-place and restore them after
    CMake rebuilds.

--- irrgo/gui/main_gui.cpp
+++ irrgo/gui/main_gui.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
-    app.setWindowIcon(QIcon(":/icons/irrgo.png"));
+    app.setWindowIcon(QIcon(":/icons/irrgo-logo-gold-v2.png"));
     MainWindow w;
     w.resize(900, 720);
     w.show();
