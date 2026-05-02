9390951412c355577a9c865fb3fb55fd9ad06871
Ben Wise <bwise.6bx1@gmail.com>
2026-05-02 08:57:45 -0400

    remove last monitored Serial.

--- in_swift/LogAdj/src/groupw/LogAdj/LEntity.java
+++ in_swift/LogAdj/src/groupw/LogAdj/LEntity.java
@@ -14,7 +14,7 @@ public abstract class LEntity extends Entity {
     static final  List<String>  monitoredSerials = // things that end up in weird locations
             new ArrayList<>(Arrays.asList(
             //"2-INF-BDE-JLTV-055",
-            "5-INF-BDE-D7-001"
+            //"5-INF-BDE-D7-001"
             ));
     static final List<String> monitoredTransports = // things that took them there
             new ArrayList<>(Arrays.asList(
